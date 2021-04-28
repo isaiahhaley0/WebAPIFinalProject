7fc2e29c312b11b9c131273b6f6d492d9fc4b07e
Josh Mansito <joshua.mansito@ucdenver.edu>
2021-04-28 16:45:48 -0600

    Temp change to server.js

--- server/server.js
+++ server/server.js
@@ -4,19 +4,17 @@ import mongoose from 'mongoose'
 import bidding from './controllers/bidding.controller'
 
 // Connection URL
-const CONNECTION_URL = 'mongodb+srv://Test:password1234@roundup.1xnaq.mongodb.net/Roundup?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 3000;
 mongoose.Promise = global.Promise
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true })
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true })
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
-const server = app.listen(PORT, (err) => {
+const server = app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
   }
-  console.info(`Server started on port: ${PORT}`)
+  console.info('Server started on port %s.', config.port)
 })
 
 bidding(server)
